d7bd5c61387f1770caabe2a4aff69e08507bdfbb
yehortpk <e2000work@gmail.com>
2024-06-21 15:06:19 +0300

    Complete tracker service track to analytics sending - Remove content.js - Add tabs and storage to permission - Remove scripting from permissions - Set minimum Chrome version to 121 (cause of tabs events) - Merge content.js event handler into background.js

--- tracker/background.js
+++ tracker/background.js
@@ -1,8 +1,64 @@
-chrome.tabs.onActivated.addListener((activeInfo) => {
-    chrome.tabs.get(activeInfo.tabId, (tab) => {
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          files: ['content.js']
+const MINIMUM_TIME_ON_PAGE_TRIGGER_MS = 5 * 1000;
+
+chrome.tabs.onActivated.addListener(
+  (result) => {
+    chrome.tabs.get(
+      result.tabId,
+      (tab) => {
+        chrome.storage.local.get(["last_tab"]).then((result) => {
+          var last_tab = result.last_tab;
+          if (last_tab != null) {
+            if ((Date.now() - last_tab.lastAccessed) > MINIMUM_TIME_ON_PAGE_TRIGGER_MS) {
+              sendTrack(last_tab.url, last_tab.lastAccessed)
+            }
+          }
         });
-      });
-});
+
+        chrome.storage.local.set({
+          "last_tab": {
+            url: tab.url,
+            lastAccessed: tab.lastAccessed
+          }
+        });
+      }
+    )
+  }
+)
+
+function formatDateToISO(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}+03:00`;
+}
+
+function sendTrack(url, lastAccessed) {
+  var data = {
+    user_id: 1,
+    url: url,
+    start: formatDateToISO(new Date(lastAccessed))
+  };
+
+  const ENDPOINT = "http://localhost:8080/track";
+
+  fetch(ENDPOINT, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(responseData => {
+      console.log('POST request successful:', responseData);
+    })
+    .catch(error => {
+      console.error('Error in POST request:', error);
+    });
+}
+
+
+
